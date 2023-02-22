2e522cd902a77bc1a8ca92f9c7156949889cc5c2
Joy Kikuvi <mwendejoy69@gmail.com>
2023-02-22 15:54:45 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Prints the characters "_putchar" on the
+* screen with the help of the function _putchar()
+* Return: 0
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+solutions to functions, nested loops

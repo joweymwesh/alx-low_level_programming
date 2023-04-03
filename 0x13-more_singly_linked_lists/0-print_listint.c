53592bcdce42151cdd2e9c77fd8fb497e5c4d9bd
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-04-03 20:58:09 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,6 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
-	
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
@@ -20,5 +18,4 @@ size_t print_listint(const listint_t *h)
 	}
 
 	return (count);
-	
 }

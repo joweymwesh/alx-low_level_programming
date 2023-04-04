c5912f6cfcb2a44ba2dd2e202b5d3265bf508d83
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-04-04 13:38:26 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,12 @@
 listint_t *get_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t **temp = head;
-	unsigned int count = 0;
+	unsigned int y = 0;
 
 	while (temp != NULL)
 	{
-		if (count == index)
-			return temp;
+		if (y == index)
+			return (**temp);
 		count++;
 		temp = temp->next;
 	}

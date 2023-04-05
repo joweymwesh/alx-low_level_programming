9cdacbe32ce54aff5eaefa17ed771f1fba6df936
Joy Kikuvi <mwendejoy69@gmail.com>
2023-04-05 05:28:49 -0700

    g

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,17 +8,17 @@
  * Return: address of the node
  */
 
-listint_t *get_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t **head = head;
 	unsigned int i = 0;
 
-	for (i = 0; head != NULL; i++)
+	if (head == NULL)
+		return (NULL)
+			for (i = 0; i < index; i++)
 	{
-		if (i == index)
-			return (head);
 
-		head = head ->next;
-	}
+		if (head == NULL)
 			return (NULL);
+	}
+			return (head);
 }

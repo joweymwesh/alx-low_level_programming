9f04f90dc3bd500e6414551078cedd24d9a16ad2
Joy Kikuvi <mwendejoy69@gmail.com>
2023-04-05 06:45:19 -0700

    up

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,30 +11,33 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *temp;
-	unsigned int i;
+	listint_t *cara, *temp;
+	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	
+	if (idx != 0)
+
+	{
+		temp = *head;
+		for (; i < idx - 1 && temp != NULL; i++)
+			temp = temp->next;
+
+		if (temp ==null )
+			return (NULL);
+	}
+	cara = malloc(sizeof(listint_t));
+	if (cara == NULL)
+		return (NULL);
+	cara->n = n;
 	if (idx == 0)
-		return (add_nodeint(head, n));
-
-	temp = *head;
-	for (i = 0; i < idx -1 && temp != NULL; i++)
-		temp = temp->next;
-
-	if (temp == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-
-	new_node->n = n;
-	new_node->next = temp->next;
-	temp->next = new_node;
-
-	return (new_node);
+	{
+		cara->next = *head;
+		*head = cara;
+		return (cara);
+	}
+	cara->next = temp->next;
+	temp->next = cara;
+	return (cara);
 }

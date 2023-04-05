d3bc9f7a1c7dd4559f9d9fbebf026956c491d9ce
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-04-05 15:31:28 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,12 @@
  * Return: address of the node
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 
 	if (head == NULL)
-		return (NULL)
+		return (NULL);
 			for (i = 0; i < index; i++)
 	{
 

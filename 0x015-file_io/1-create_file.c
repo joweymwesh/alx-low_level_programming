d68ecfee3bf49cef47f261cc562ca602e1f3d205
Joy Kikuvi <mwendejoy69@gmail.com>
2023-04-13 07:17:22 -0700

    under the snow

--- 0x015-file_io/1-create_file.c
+++ 0x015-file_io/1-create_file.c
@@ -10,12 +10,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int op, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++
+		for (len = 0; text_content[len] len++);
 	}
 
 	op = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);

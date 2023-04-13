06c70289073e7cd39e9f2729c8a4ed5cda9108e7
Joy Kikuvi <mwendejoy69@gmail.com>
2023-04-13 06:28:45 -0700

    tread lightly

---
+++ 0x015-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: file to be read
+ * @letters : number of letters to read and print
+ * @Return : number of letters read and printed.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	sssize_t op, r, t;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O-RDONLY);
+	r = read(op, buffer , letters);
+	t = write(STDOUT_FILENO,buffer, r);
+
+	if (op == -1 || r ==-1 || t == -1 || t != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(op);
+
+	return (t);
+}

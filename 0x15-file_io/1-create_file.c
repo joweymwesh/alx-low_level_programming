b9205b78c6b6654b2d9f3d19ad66159f6268f58b
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-04-14 23:27:08 +0300

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/* create_file - a file is created
+/**create_file - a file is created
  * @filename : pointer to the name of file to create
  * text-content : a pointer to a string to write to the file
  * Return: if it fails - -1, otherwise - 1
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++);
+		for (len = 0; text_content[len]; 
+		     len++);
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);

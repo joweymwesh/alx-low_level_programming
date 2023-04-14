89e8ec5596fba6a4f18915ceeb1f35c823ec8ea7
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-04-14 23:25:26 +0300

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] len++);
+		for (len = 0; text_content[len]; len++);
 	}
 
-	op = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(op, text_content, len);
 
 	if  (op == -1 || w == -1)

2b4c975cbda118ca8a5ec304e3b9ce67ccb3969c
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-05-04 23:49:34 +0300

    print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n -  number
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int i, count = 0;
+
+	for (i = 63; i >= 0; i++)
+	{
+		temp = n >> i;
+		if (temp & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+
+		else if (count)
+			_putchar('0');
+	}
+
+	if (!count)
+		_putchar('0');
+}
